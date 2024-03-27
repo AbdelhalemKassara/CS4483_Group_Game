8c253c84397f1d58d8bfbed6d040481c1ea5cb06
J-Yong-22 <johnson.yong.135x@gmail.com>
2024-03-27 14:03:58 -0400

    Timer

--- Assets/Scripts/DashboardManager.cs
+++ Assets/Scripts/DashboardManager.cs
@@ -8,6 +8,8 @@ public class DashboardManager : MonoBehaviour
 {
     public TextMeshProUGUI curGear;
     public TextMeshProUGUI curRpm;
+    public TextMeshProUGUI timer;
+    float elapsedTime;
     
     public virtual void setGear(int gear)
     {
@@ -25,5 +27,12 @@ public class DashboardManager : MonoBehaviour
     public virtual void setRpm(float rpm)
     {
         curRpm.text = "MaxRPM: 90000 \nRPM: " + Convert.ToString(rpm);
+    }
+
+    void Update(){
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
