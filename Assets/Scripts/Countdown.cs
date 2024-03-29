fdc168d3ea50a79fee90c51f3df4e4e0534adccd
LiQian Zhou <90808836+edwin20151@users.noreply.github.com>
2024-03-29 19:34:21 -0400

    the start racing

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private Text CountDown;
+    [SerializeField] private GameObject Dashboard;
+
+    private int Num = 5;
+    private float countdownDelay = 1f; // Delay between counts
+
+    void Start()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        while (Num > 0)
+        {
+            CountDown.text = Num.ToString();
+            yield return new WaitForSeconds(countdownDelay);
+            Num--;
+        }
+
+        if(Num == 0){
+            CountDown.text = "Start";
+            gameObject.SetActive(false);
+            CountDown.gameObject.SetActive(false);
+            Dashboard.gameObject.SetActive(true);
+        }
+        
+        // Optionally, you can add code here to handle what happens after the countdown finishes.
+    }
+}
--- Assets/Scripts/selected.cs
+++ Assets/Scripts/selected.cs
@@ -15,8 +15,17 @@ public class selected : MonoBehaviour
     private void SelectionCar(int index)
     {
        
+<<<<<<< Updated upstream
         for(int i =0; i<transform.childCount; i++){
 
+=======
+        for(int i =0; i<transform.childCount; i++)
+        {
+
+            if(i==index){
+            cameraManager.setCurCar(transform.GetChild(i).gameObject);
+            }
+>>>>>>> Stashed changes
             transform.GetChild(i).gameObject.SetActive(i== index);
         }
     }
