63f002b992f5ed41c913c1ae821d39ef8cca78cc
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-03-31 22:33:04 -0400

    moved the dashboard code to the dashboard manger from the CarController

--- Assets/Scripts/DashboardManager.cs
+++ Assets/Scripts/DashboardManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Car;
 using TMPro;
 using UnityEngine;
 
@@ -10,7 +11,8 @@ public class DashboardManager : MonoBehaviour
     public TextMeshProUGUI curRpm;
     public TextMeshProUGUI timer;
     public TextMeshProUGUI speed;
-    
+
+    private CarController _carController;
     float elapsedTime;
     
     public virtual void setGear(int gear)
@@ -26,11 +28,16 @@ public class DashboardManager : MonoBehaviour
         }
     }
 
+
     public float GetElapsedTime()
     {
         return elapsedTime;
     }
 
+    public void setCurCarController(CarController carController)
+    {
+        _carController = carController;
+    }
     public virtual void setSpeed(float speed)
     {
         this.speed.text = Convert.ToString(speed) + " KM/H";
@@ -41,6 +48,11 @@ public class DashboardManager : MonoBehaviour
     }
 
     void Update(){
+        setRpm(_carController.getRpm());
+        setSpeed(_carController.getSpeed());
+        setGear(_carController.getGear());
+        
+        
         elapsedTime += Time.deltaTime;
         int minutes = Mathf.FloorToInt(elapsedTime / 60);
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
