fb47a31bddcf2b921f09c205f0b074ad525f6295
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-04-01 23:37:20 -0400

    add basic rotation that is hard coded

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public enum CameraModes : int
 {
     one= 1,
-    two = 2
+    two = 2,
+    three = 3
 }
 [Serializable]
 public struct CameraModeSetings
@@ -29,8 +30,11 @@ public class CameraManager : MonoBehaviour
 
     protected int curCamSetting = 0;
     // Start is called before the first frame update
-
-
+    private CarInputController _carInputController;
+    void Start()
+    {
+        _carInputController = curCar.GetComponent<CarInputController>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -39,16 +43,34 @@ public class CameraManager : MonoBehaviour
         switch (cameraModeSet.mode)
         {
             case CameraModes.one:
-                transform.position = Vector3.Lerp(transform.position, curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z)), Time.deltaTime);// slowely follows the car
+                //right now it is learping from the old camera positon to the new position.
+                transform.position = Vector3.Lerp(transform.position, RotatePointAroundPivot(curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z)), curCar.transform.position, new Vector3(0.0f, 90.0f, 0.0f)), Time.deltaTime);// slowely follows the car
                 transform.LookAt(curCar.transform);
+
                 Camera.main.fieldOfView = cameraModeSet.fov;
                 break;
             case CameraModes.two:
-                transform.position = curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z)); // stays behind the car
+                transform.position = RotatePointAroundPivot(curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z)), curCar.transform.position, new Vector3(0.0f, 90.0f, 0.0f)); // stays behind the car
                 transform.rotation = curCar.transform.rotation;
+                transform.Rotate(new Vector3(0.0f, -90.0f, 0.0f));
+
                 Camera.main.fieldOfView = cameraModeSet.fov; 
                 break;
+            case CameraModes.three:
+                transform.position = Vector3.ClampMagnitude(Vector3.Lerp(transform.position, curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z)), Time.deltaTime), (curCar.transform.position + curCar.transform.TransformDirection(new Vector3(cameraModeSet.x, cameraModeSet.y, cameraModeSet.z))).magnitude);// slowely follows the car
+                transform.LookAt(curCar.transform);
+                
+                Camera.main.fieldOfView = cameraModeSet.fov;
+                
+                break;
         }
+    }
+
+    private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot,Vector3 angles) {
+        Vector3 dir = point - pivot; 
+        dir = Quaternion.Euler(angles) * dir; 
+        point = dir + pivot; 
+        return point; 
     }
 
     public void setCurCar(GameObject obj)
